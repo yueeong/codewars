ce1e0d4ab73ee45aaa983a560913395fbe1c3bb0
YuEe Ong <yueeong@cisco.com>
2016-11-28 19:19:46 -0600

    beautify

--- accum.js
+++ accum.js
@@ -4,7 +4,7 @@ function accum(s) {
 
         newstr += s[x].toUpperCase();
         newstr += s[x].toLocaleLowerCase().repeat(parseInt(x));
-        if ( x < s.length-1){
+        if (x < s.length - 1) {
             newstr += "-";
         }
 
@@ -15,49 +15,58 @@ function accum(s) {
 function getMiddle(s) {
     if (s.length % 2 === 0) {
         console.log("even");
-        midIdx  = s.length/2;
-        return s[midIdx-1]+s[midIdx];
+        midIdx = s.length / 2;
+        return s[midIdx - 1] + s[midIdx];
 
-    }
-    else {
-        midIdx  = Math.floor(s.length/2);
+    } else {
+        midIdx = Math.floor(s.length / 2);
         return s[midIdx];
     }
 }
 
 function getSum(a, b) {
-    function add(x,y) {return x+y;}
+    function add(x, y) {
+        return x + y;
+    }
     var arr = [];
 
-    if ( a == b) {
+    if (a == b) {
         return a;
-    }
-    else if (a < b) {
+    } else if (a < b) {
         console.log("a less b");
 
-        for ( count = a; count <= b; count++ ){
+        for (count = a; count <= b; count++) {
             arr.push(count);
         }
-        return arr.reduce(add,0);
-    }
-    else if (a > b) {
+        return arr.reduce(add, 0);
+    } else if (a > b) {
         console.log("a gret b");
 
-        for ( count = b; count <= a; count++ ){
+        for (count = b; count <= a; count++) {
             arr.push(count);
         }
-        return arr.reduce(add,0);
+        return arr.reduce(add, 0);
     }
 }
 
-console.log(getSum(6,-1));
+console.log(getSum(6, -1));
 
-function list(names){
-    function catThemUp(a,b) { return a + ',' + b; }
+function list(names) {
+    function catThemUp(a, b) {
+        return a + ',' + b;
+    }
     for (var x in names) {
         console.log(names[x].name);
     }
     return "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
 }
-var names = [{name: 'Bart'},{name: 'Lisa'},{name: 'Maggie'},{name: 'Homer'}];
+var names = [{
+    name: 'Bart'
+}, {
+    name: 'Lisa'
+}, {
+    name: 'Maggie'
+}, {
+    name: 'Homer'
+}];
 console.log(list(names));
